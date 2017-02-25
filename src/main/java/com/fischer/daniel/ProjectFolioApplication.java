f4ba5bd6837be5bf5d39c12e97ee857031c2e441
Daniel Fischer <daniel.fische@gmail.com>
2017-02-25 12:07:01 -0500

    initial spring boot application

---
+++ src/main/java/com/fischer/daniel/ProjectFolioApplication.java
@@ -0,0 +1,12 @@
+package com.fischer.daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFolioApplication.class, args);
+	}
+}
