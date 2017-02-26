0682596838714c7d7579f702abb85eb842c2eeae
Daniel Fischer <fische.daniel@gmail.com>
2017-02-26 11:56:15 -0500

    temp remove spring security

--- src/main/java/com/fischer/daniel/web/TestEndpointController.java
+++ src/main/java/com/fischer/daniel/web/TestEndpointController.java
@@ -1,13 +1,16 @@
 package com.fischer.daniel.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 public class TestEndpointController {
 
-  @RequestMapping("/greeting")
+  @RequestMapping(path = "/greeting", method = RequestMethod.GET)
   public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
     return name + "greeting";
   }
